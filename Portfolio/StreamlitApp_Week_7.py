a40e5e02a91f6cccf8c3e26d7ffe946e35c73148
jadyneberly <j.r.eberly@tcu.edu>
2026-03-26 16:23:13 -0500

    Refactor AWS model endpoint and input keys

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -60,8 +60,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_pair_model.tar.gz',
-        "keys": ["MPWR", "AAPL"],
-        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["MPWR", "AAPL"]]
+        "keys": ["GOOGL", "GOOG"],
+        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["GOOGL", "GOOG"]]
 }
 
 def load_pipeline(_session, bucket, key):
@@ -161,7 +161,3 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
-
-
-
-
